97d059910fadefac89763536afd1dc7a1f41f9d0
maulikatwork <107258521+maulikatwork@users.noreply.github.com>
2025-05-14 17:48:26 +0530

    Added Types

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-native-unity-launcher' {
+  /**
+   * Launches the Unity application
+   * @returns A Promise that resolves when Unity is launched successfully
+   */
+  export function launchUnity(): Promise<void>;
+} 
